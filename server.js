9e921a76eb54b4151b270a7b3516429283d97f18
C.Nguyen <cdkedits@gmail.com>
2019-07-09 18:10:34 -0700

    set up server.js to connect with mongoose and routes

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require(`express`)
+const app = express()
+const { join } = require(`path`)
+
+app.use(express.static(join(__dirname, `/client`)))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+require(`./routes`)(app)
+
+require(`mongoose`).connect(`mongodb://localhost/__db`)
+  .then(_ => app.listen(3001))
+  .catch(e => console.log(e))
