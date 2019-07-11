38da07e3e07ba8cd7ed9db80b7b207d7b6c8eb91
dien <iamdiennn@gmail.com>
2019-07-10 21:50:13 -0700

    basic setup

---
+++ client/src/Pages/Thread/Thread.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react'
+
+
+
+const ThreadApp = _ => {
+const [threads, setThreads] = useState([])
+const [comments, setComments] = useState([])
+
+    return(
+        <div>
+
+        </div>
+    )
+}
