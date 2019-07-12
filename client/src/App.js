efa282226beaa2776b4deb29c62022e16c31bd0a
Pandapoc <45634681+Pandapoc@users.noreply.github.com>
2019-07-11 18:24:25 -0700

    js standard formating

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom'
-import NavBar from "./components/NavBar"
-import Threads from "./Pages/Thread/Thread.js"
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Threads from './Pages/Thread/Thread.js'
 
 const App = _ => {
-  return(
+  return (
     <>
-    <Router>
-      <NavBar/>
-      <Route path='/threads' render={_ => (
-            <Threads
-            />
-            )} />
-    </Router>
+      <Router>
+        <NavBar />
+        <Route path='/threads' render={_ => (
+          <Threads
+          />
+        )} />
+      </Router>
     </>
   )
 }
 
-export default App;
+export default App
