7c0eddac9377bdb332aa68743bb5313f0b01ded4
Pandapoc <45634681+Pandapoc@users.noreply.github.com>
2019-07-17 15:21:21 -0700

    test

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import LoginPage from './Pages/Login'
 import SignupPage from './components/SignupForm'
 import axios from 'axios'
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar/Navbar.js'
 import Search from './Pages/Search'
 
 const App = _ => {
