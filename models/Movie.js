37fc7c2cff4f33d5029b432391ec0014f0c5ced4
SPFlores <sflores92886@gmail.com>
2019-07-16 18:56:08 -0700

    movie model

--- models/Movie.js
+++ models/Movie.js
@@ -1,3 +1,17 @@
 module.exports = (Schema, model) => model(`Movie`, new Schema({
+  vote_count: String,
+  tmdb_id: String,
+  vote_average: String,
+  title: String,
+  popularity: String,
+  poster_path: String,
+  original_language: String,
+  original_title: String,
+  genre_ids: String,
+  adult: String,
+  overview: String,
+  release_date: String
 
+  // cast
+  // director
 }))
