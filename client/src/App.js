58d4d632724fa8d1c89179beb0708343f01ed6ea
Pandapoc <45634681+Pandapoc@users.noreply.github.com>
2019-07-16 20:24:40 -0700

    removed threads

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
-import Threads from './Pages/Thread/Thread.js'
 
 const App = _ => {
   return (
     <>
       <Router>
         <NavBar />
-        <Route path='/threads' render={_ => (
-          <Threads
-          />
-        )} />
+        <Route />
       </Router>
     </>
   )
