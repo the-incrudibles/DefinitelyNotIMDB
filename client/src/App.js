4520ad16ed9af30a2ce5e9123440eab61182610f
SPFlores <sflores92886@gmail.com>
2019-07-16 21:16:08 -0700

    search page, some navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Threads from './Pages/Thread/Thread.js'
+import Search from './Pages/Search'
 
 const App = _ => {
   return (
@@ -11,6 +12,9 @@ const App = _ => {
         <Route path='/threads' render={_ => (
           <Threads
           />
+        )} />
+        <Route path='/search' render={_ => (
+          <Search />
         )} />
       </Router>
     </>
--- client/src/Pages/Search/Search.js
+++ client/src/Pages/Search/Search.js
@@ -6,42 +6,42 @@ import FormControl from '@material-ui/core/FormControl'
 import Select from '@material-ui/core/Select'
 import NativeSelect from '@material-ui/core/NativeSelect'
 import InputBase from '@material-ui/core/InputBase'
-import Navbar from '../../components/NavBar.js'
 
 const Search = _ => {
-  const SearchInput = withStyles(theme => ({
-    root: {
-      'label + &': {
-        marginTop: theme.spacing(3)
-      }
-    },
-    input: {
-      borderRadius: 4,
-      position: 'relative',
-      backgroundColor: theme.palette.background.paper,
-      border: '1px solid #ced4da',
-      fontSize: 16,
-      padding: '10px 26px 10px 12px',
-      transition: theme.transitions.create(['border-color', 'box-shadow']),
-      fontFamily: [
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"'
-      ].join(','),
-      '&:focus': {
-        borderRadius: 4,
-        borderColor: '#80bdff',
-        boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)'
-      }
-    }
-  }))(InputBase)
+  // const SearchInput = withStyles(theme => ({
+  //   root: {
+  //     'label + &': {
+  //       // marginTop: theme.spacing(3)
+  //     }
+  //   },
+  //   input: {
+  //     borderRadius: 4,
+  //     position: 'relative',
+  //     backgroundColor: theme.palette.background.paper,
+  //     border: '1px solid #ced4da',
+  //     fontSize: 16,
+  //     padding: '10px 26px 10px 12px',
+  //     transition: theme.transitions.create(['border-color', 'box-shadow']),
+  //     fontFamily: [
+  //       '-apple-system',
+  //       'BlinkMacSystemFont',
+  //       '"Segoe UI"',
+  //       'Roboto',
+  //       '"Helvetica Neue"',
+  //       'Arial',
+  //       'sans-serif',
+  //       '"Apple Color Emoji"',
+  //       '"Segoe UI Emoji"',
+  //       '"Segoe UI Symbol"'
+  //     ].join(','),
+  //     '&:focus': {
+  //       borderRadius: 4,
+  //       borderColor: '#80bdff',
+  //       boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)'
+  //     }
+  //   }
+  // }))(InputBase)
+
   const [age, setAge] = useState('')
   const handleChange = event => {
     setAge(event.target.value)
@@ -49,27 +49,23 @@ const Search = _ => {
 
   return (
     <>
-      <Navbar />
       <input type='text' name='search' id='search' />
-
-      <form autoComplete='off'>
+      <br />
+      <form>
         <FormControl>
-          <InputLabel htmlFor='age-customized-select'>Age</InputLabel>
+          <InputLabel htmlFor='category'>Search Category</InputLabel>
           <Select
             value={age}
             onChange={handleChange}
-            input={<SearchInput name='age' id='age-customized-select' />}
+          // input={<SearchInput name='search' id='category' />}
           >
-            <MenuItem value=''>
-              <em>None</em>
-            </MenuItem>
+            <MenuItem disabled />
             <MenuItem value='movie'>Movie</MenuItem>
             <MenuItem value='celebrity'>Celebrity</MenuItem>
             <MenuItem value='genre'>Genre</MenuItem>
           </Select>
         </FormControl>
       </form>
-  )
 
       <div>
         <h6>Movies/Celebrities/Genres</h6>
