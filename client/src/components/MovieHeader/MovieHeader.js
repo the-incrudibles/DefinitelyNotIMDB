8c83f48ebddced9cb747d9cff8bb268b3377a53b
dien <iamdiennn@gmail.com>
2019-07-18 15:34:51 -0700

    added cast tile for example

--- client/src/components/MovieHeader/MovieHeader.js
+++ client/src/components/MovieHeader/MovieHeader.js
@@ -3,13 +3,16 @@ import axios from 'axios'
 import { makeStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
+import Card from '@material-ui/core/Card'
+import CardMedia from '@material-ui/core/CardMedia'
+import CardContent from '@material-ui/core/CardContent'
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import Fab from '@material-ui/core/Fab'
+import AddIcon from '@material-ui/icons/Add'
+import tileData from './tileData.js'
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
 
 
 const useStyles = makeStyles(theme => ({
@@ -19,10 +22,29 @@ const useStyles = makeStyles(theme => ({
     card: {
         maxWidth: 350
     },
+    rootTwo: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+        backgroundColor: theme.palette.background.paper,
+      },
     media:{
         maxHeight: 200,
         maxWidth:200
-    }
+    },
+    gridList: {
+        flexWrap: 'nowrap',
+        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+        transform: 'translateZ(0)',
+      },
+      title: {
+        color: theme.palette.primary.light,
+      },
+      titleBar: {
+        background:
+          'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+      },
   }))
 
 
@@ -78,6 +100,15 @@ const useStyles = makeStyles(theme => ({
             </Typography> 
         </Grid>
       </Paper>
+      <div className={classes.rootTwo}>
+      <GridList className={classes.gridList} cols={2.5}>
+        {tileData.map(tile => (
+          <GridListTile key={tile.img}>
+            <img src={tile.img} alt={tile.title} />
+          </GridListTile>
+        ))}
+      </GridList>
+      </div>
     </div>
     )
   }
