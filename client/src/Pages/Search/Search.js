b6712827e446cd39ed5ef0f151a6f85c43f06622
SPFlores <sflores92886@gmail.com>
2019-07-18 19:58:06 -0700

    dropdown on search, text input

--- client/src/Pages/Search/Search.js
+++ client/src/Pages/Search/Search.js
@@ -1,70 +1,39 @@
-import React, { useState } from 'react'
-import { withStyles } from '@material-ui/core/styles'
-import InputLabel from '@material-ui/core/InputLabel'
-import MenuItem from '@material-ui/core/MenuItem'
-import FormControl from '@material-ui/core/FormControl'
-import Select from '@material-ui/core/Select'
-import NativeSelect from '@material-ui/core/NativeSelect'
-import InputBase from '@material-ui/core/InputBase'
+import React, { useState, useRef } from 'react'
 
 const Search = _ => {
-  // const SearchInput = withStyles(theme => ({
-  //   root: {
-  //     'label + &': {
-  //       // marginTop: theme.spacing(3)
-  //     }
-  //   },
-  //   input: {
-  //     borderRadius: 4,
-  //     position: 'relative',
-  //     backgroundColor: theme.palette.background.paper,
-  //     border: '1px solid #ced4da',
-  //     fontSize: 16,
-  //     padding: '10px 26px 10px 12px',
-  //     transition: theme.transitions.create(['border-color', 'box-shadow']),
-  //     fontFamily: [
-  //       '-apple-system',
-  //       'BlinkMacSystemFont',
-  //       '"Segoe UI"',
-  //       'Roboto',
-  //       '"Helvetica Neue"',
-  //       'Arial',
-  //       'sans-serif',
-  //       '"Apple Color Emoji"',
-  //       '"Segoe UI Emoji"',
-  //       '"Segoe UI Symbol"'
-  //     ].join(','),
-  //     '&:focus': {
-  //       borderRadius: 4,
-  //       borderColor: '#80bdff',
-  //       boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)'
-  //     }
-  //   }
-  // }))(InputBase)
+  const [searchState, setSearchState] = useState({
+    searchArea: 'nothing',
+    searchTerm: ''
+  })
 
-  const [age, setAge] = useState('')
-  const handleChange = event => {
-    setAge(event.target.value)
+  const searchTerm = useRef()
+
+  searchState.handleSearchArea = event => {
+    setSearchState({ ...searchState, searchArea: event.target.value })
+  }
+
+  searchState.buttonClick = event => {
+    event.preventDefault()
+    console.log(searchState.searchArea)
+    console.log(searchTerm.current.value)
   }
 
   return (
     <>
-      <input type='text' name='search' id='search' />
+      <input type='text' name='search' id='searchTerm' ref={searchTerm} />
       <br />
       <form>
-        <FormControl>
-          <InputLabel htmlFor='category'>Search Category</InputLabel>
-          <Select
-            value={age}
-            onChange={handleChange}
-          // input={<SearchInput name='search' id='category' />}
-          >
-            <MenuItem disabled />
-            <MenuItem value='movie'>Movie</MenuItem>
-            <MenuItem value='celebrity'>Celebrity</MenuItem>
-            <MenuItem value='genre'>Genre</MenuItem>
-          </Select>
-        </FormControl>
+        <select
+          value={searchState.searchArea}
+          onChange={searchState.handleSearchArea}
+          id='searchArea'
+        >
+          <option />
+          <option value='movie'>Movie</option>
+          <option value='celebrity'>Celebrity</option>
+          <option value='genre'>Genre</option>
+        </select>
+        <button id='someBtn' onClick={searchState.buttonClick}>Click me</button>
       </form>
 
       <div>
