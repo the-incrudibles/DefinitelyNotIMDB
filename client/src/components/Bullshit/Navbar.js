2a93ff15b9499f10d4ec2681cf4088fc2f6a7adf
C.Nguyen <cdkedits@gmail.com>
2019-07-20 13:58:42 -0700

    added functionality to hide admin button from navbar

--- client/src/components/Bullshit/Navbar.js
+++ client/src/components/Bullshit/Navbar.js
@@ -80,10 +80,15 @@ const Navbar = _ => {
       </List>
       <Divider />
       <List>
-        <ListItem>
-          <i className="material-icons">vpn_key</i>
-          <Link to="/account" className="drawerLink"><ListItemText primary="Admin" /></Link>
-        </ListItem>
+        {
+          localStorage.getItem('auth') ?
+            <>
+              <ListItem>
+                <i className="material-icons">vpn_key</i>
+                <Link to="/account" className="drawerLink"><ListItemText primary="Admin" /></Link>
+              </ListItem>
+            </> : null
+        }
         <ListItem>
           <i className="material-icons">account_box</i>
           <Link to="/account" className="drawerLink"><ListItemText primary="My Account" /></Link>
