3ffd6aed8267c4924c8ef1a0794c3a0dca27942e
C.Nguyen <cdkedits@gmail.com>
2019-07-19 18:18:24 -0700

    added useRef import to LoginForm

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import Typography from '@material-ui/core/Typography'
 // Text Input Imports:
 import TextField from '@material-ui/core/TextField'
