c441f91b4e30803911f60555f58088cd7784f39e
C.Nguyen <cdkedits@gmail.com>
2019-07-23 01:05:27 -0700

    adjusted textfield incompleteSearch case in search.js

--- client/src/Pages/Search/Search.js
+++ client/src/Pages/Search/Search.js
@@ -120,7 +120,6 @@ const TestSearch = _ => {
               margin='normal'
               variant='outlined'
               inputRef={searchTerm}
-              value={searchTerm.current.value}
               error id
             />
           </>
