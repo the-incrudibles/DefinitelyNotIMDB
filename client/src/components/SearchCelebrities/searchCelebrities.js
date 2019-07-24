f3f9227b99cf35f1defc4ac001d5170249ac5e9b
SPFlores <sflores92886@gmail.com>
2019-07-23 17:39:00 -0700

    standard fixes

--- client/src/components/SearchCelebrities/searchCelebrities.js
+++ client/src/components/SearchCelebrities/searchCelebrities.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import SearchContext from '../../utils/searchContext'
 import Typography from '@material-ui/core/Typography'
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import Button from '@material-ui/core/Button'
 import Placeholder from '../../images/placeholder_poster.jpg'
 
 const searchCelebrities = _ => {
@@ -17,11 +17,11 @@ const searchCelebrities = _ => {
             <Card key={celeb.id} onClick={_ => searchForCeleb(celeb.id)} className='resultsDiv'>
               <CardActionArea>
                 <CardContent>
-                  <Typography gutterBottom variant="h6" component="h2">
+                  <Typography gutterBottom variant='h6' component='h2'>
                     {celeb.name}
                   </Typography>
                   {
-                    celeb.profile_path ? <img className="resultsPoster" src={`https://image.tmdb.org/t/p/original${celeb.profile_path}`} alt={celeb.name} /> : <img className="resultsPoster" src={Placeholder} alt={celeb.name} />
+                    celeb.profile_path ? <img className='resultsPoster' src={`https://image.tmdb.org/t/p/original${celeb.profile_path}`} alt={celeb.name} /> : <img className='resultsPoster' src={Placeholder} alt={celeb.name} />
                   }
                 </CardContent>
               </CardActionArea>
--- client/src/components/SearchMovie/searchMovie.js
+++ client/src/components/SearchMovie/searchMovie.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import SearchContext from '../../utils/searchContext'
 import Typography from '@material-ui/core/Typography'
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import Button from '@material-ui/core/Button'
 import Placeholder from '../../images/placeholder_poster.jpg'
 
 const searchMovie = _ => {
@@ -20,14 +20,14 @@ const searchMovie = _ => {
             <Card key={movie.id} className='resultsDiv' onClick={_ => searchForMovie(movie.id)} >
               <CardActionArea>
                 <CardContent>
-                  <Typography gutterBottom variant="h6" component="h2">
+                  <Typography gutterBottom variant='h6' component='h2'>
                     {movie.title ? movie.title : movie.name}
                   </Typography>
                   <Typography variant='body2' color='textSecondary' component='p'>
                     {movie.genres}
                   </Typography>
                   {
-                    movie.poster_path ? <img className="resultsPoster" src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.title} /> : <img className="resultsPoster" src={Placeholder} alt={movie.title} />
+                    movie.poster_path ? <img className='resultsPoster' src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.title} /> : <img className='resultsPoster' src={Placeholder} alt={movie.title} />
                   }
                 </CardContent>
               </CardActionArea>
