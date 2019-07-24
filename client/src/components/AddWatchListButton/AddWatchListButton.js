31f7b55449bd4a8dc2cd14fe38f051bb1ec2fab5
Nuo <nuot@uci.edu>
2019-07-23 17:52:27 -0700

    add watchlist page

--- client/src/components/AddWatchListButton/AddWatchListButton.js
+++ client/src/components/AddWatchListButton/AddWatchListButton.js
@@ -1,33 +1,18 @@
-import React, { Component,useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import Fab from '@material-ui/core/Fab'
 import AddIcon from '@material-ui/icons/Add'
 
-class AddWatchListButton extends Component {
-    state = {
-      title: '',
-      movie: {},
-      watchlist: []
-    }
-
-    
-    handleAddWatchList = event => {
-        console.log('button works')
-        }
-        render(){
-        return (
-        <div className="addWatchlistButton" onClick ={this.handleAddWatchList}>
-            <Fab color="default" aria-label="Add" size="small">
-                <AddIcon />
-            </Fab>
-        </div>
-    )
-}
+const AddWatchListButton = _ => {
+  return (
+    <div className='addWatchlistButton'>
+      <Fab color='default' aria-label='Add' size='small'>
+        <AddIcon />
+      </Fab>
+    </div>
+  )
 }
 
 export default AddWatchListButton
-
-
-
 
 
 
