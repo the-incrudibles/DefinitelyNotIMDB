37fbe16ac4318e2f7b60f44cd8a87ee3c89145a5
C.Nguyen <cdkedits@gmail.com>
2019-07-24 21:23:55 -0700

    added tvshow info page

---
+++ client/src/components/TVShowComments/commentData.js
@@ -0,0 +1,19 @@
+const commentData = [
+  {
+    name: 'Dien',
+    comment: 'Show is alright',
+    report: false
+  },
+  {
+    name: 'Connor',
+    comment: 'Show is bomb diggitay',
+    report: false
+  },
+  {
+    name: 'Nick',
+    comment: 'Show is bullshit',
+    report: false
+  }
+]
+
+export default commentData
