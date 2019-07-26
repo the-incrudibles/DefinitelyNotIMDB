6d09b398b3e2b760f31a125d68dcc8e4b57750bf
SPFlores <sflores92886@gmail.com>
2019-07-25 21:54:51 -0700

    guidebox API work

--- client/src/components/GuideBoxApi/guideboxApi.js
+++ client/src/components/GuideBoxApi/guideboxApi.js
@@ -1,58 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const GuideBox = _ => {
-  const [guideState, setGuideState] = useState({})
+  const [guideState, setGuideState] = useState({
+    GBID: '',
+    hasRun: false
+  })
 
-  guideState.getGuideBox = _ => {
+  useEffect(_ => {
+    getGuideBox()
+  }, [])
+
+  const getGuideBox = _ => {
     axios.get(`http://api-public.guidebox.com/v2/search?api_key=${process.env.REACT_APP_GUIDEBOX_APIKEY}&type=movie&field=id&id_type=themoviedb&query=${localStorage.getItem('movieID')}`)
-      .then(data => {
-        console.log(data)
-      })
+      .then(({ data: movie }) => setGuideState({ ...guideState, GBID: movie.id, hasRun: true }))
       .catch(e => console.log(e))
-    //     axios.get(`http://api-public.guidebox.com/v2/search?type=movie&field=title&include_preorders=true&include_in_theaters=true&query=${searchTerm.current.value}&api_key=`)
-    //       .then(({ data }) => {
-    //         searchTerm.current.value = ''
-    //         console.log(data)
-
-    //         var preOrder = data.results.filter(
-    //           e => e.pre_order === true
-    //         )
-    //         console.log(preOrder)
   }
 
+  useEffect(_ => {
+    axios.get(`http://api-public.guidebox.com/v2/movies/${guideState.GBID}/?include_links=true&api_key=${process.env.REACT_APP_GUIDEBOX_APIKEY}`)
+      .then(({ data }) => console.log(data))
+  }, [guideState.hasRun])
+
   return (
-    <h1>this is the guidebox thing</h1>
+    <>
+      <h1>this is the guidebox thing</h1>
+    </>
   )
 }
 
 export default GuideBox
-
-// searchState.buttonClick = e => {
-//   e.preventDefault()
-//   // movie search
-//   if (searchState.searchArea === 'movie' && searchTerm.current.value !== '') {
-//     axios.get(`http://api-public.guidebox.com/v2/search?type=movie&field=title&include_preorders=true&include_in_theaters=true&query=${searchTerm.current.value}&api_key=${process.env.REACT_APP_GUIDEBOX_APIKEY}`)
-//       .then(({ data }) => {
-//         searchTerm.current.value = ''
-//         console.log(data)
-
-//         var preOrder = data.results.filter(
-//           e => e.pre_order === true
-//         )
-//         console.log(preOrder)
-
-//       })
-//       .catch(e => console.log(e))
-
-//     // show search
-//   } else if (searchState.searchArea === 'tv' && searchTerm.current.value !== '') {
-//     axios.get(`http://api-public.guidebox.com/v2/search?api_key=3228a71302f7780be2e65d84356cadc6d85b7a4e&type=show&field=title&query=${searchTerm.current.value}`)
-//       .then(({ data }) => {
-//         searchTerm.current.value = ''
-//         console.log(data)
-//       })
-//       .catch(e => console.log(e))
-
-//   }
-// }
