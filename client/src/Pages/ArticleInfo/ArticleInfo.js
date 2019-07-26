446bcd63b44780c903aec09a22536c36493acc1d
C.Nguyen <cdkedits@gmail.com>
2019-07-26 15:39:40 -0700

    ArticleInfo page is done!!

--- client/src/Pages/ArticleInfo/ArticleInfo.js
+++ client/src/Pages/ArticleInfo/ArticleInfo.js
@@ -1,10 +1,37 @@
 import React, { useEffect, useState } from 'react'
-// import MainContext from '../../utils/mainContext'
-// import Article from '../../components/Article'
+import { makeStyles } from '@material-ui/core/styles'
 import MainArticles from '../../utils/MainArticles.js'
-// import Typography from '@material-ui/core/Typography'
+import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import GuideBox from '../../components/GuideBoxApi'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: 15,
+    height: "100%"
+  },
+  card: {
+    maxWidth: 350
+  },
+  media: {
+    maxHeight: 200,
+    maxWidth: 200
+  },
+  gridList: {
+    flexWrap: 'nowrap',
+    transform: 'translateZ(0)'
+  },
+  title: {
+    color: theme.palette.primary.light
+  },
+  gridHeight: {
+    height: "100%"
+  }
+}))
 
 const ArticleInfo = _ => {
+  const classes = useStyles()
   const [mainState, setMainState] = useState({
     headline: '',
     summary: '',
@@ -13,9 +40,8 @@ const ArticleInfo = _ => {
 
   const searchForArticle = _ => {
     MainArticles.getArticle(localStorage.getItem('articleID'))
-      .then(article => {
-        // setMainState({ ...mainState, headline: data.headline })
-        console.log(article)
+      .then(({ data }) => {
+        setMainState({ ...mainState, headline: data.headline, summary: data.summary, image: data.image })
       })
       .catch(e => console.log(e))
   }
@@ -26,7 +52,28 @@ const ArticleInfo = _ => {
 
   return (
     <>
-      hello
+      <div>
+        <Paper className={classes.root}>
+          <div>
+            <Grid container spacing={1}>
+              <Grid item xs={6}>
+                <img className='movieImg' src={mainState.image} alt='' />
+              </Grid>
+              <Grid item xs={6}>
+                <Typography variant='h6' component='h3' className="movieHeaderText">
+                  {mainState.headline}
+                </Typography>
+              </Grid>
+              <Typography variant='h6' gutterBottom>
+                <strong>Here's the scoop:</strong>
+              </Typography>
+              <Typography>
+                {mainState.summary}
+              </Typography>
+            </Grid>
+          </div>
+        </Paper>
+      </div>
     </>
   )
 }
