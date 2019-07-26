0614a279e010c6396eeec4817da3c59459ba97d4
Mahya <safavis@uci.edu>
2019-07-26 11:38:21 -0700

    fixing the get watchlist route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,14 +15,17 @@ module.exports = app => {
       if (e) throw e
       User.authenticate()(req.body.username, req.body.password, (e, user) => {
         if (e) throw e
-        res.json({ isLoggedIn: !!user, user: user.username, token: jwt.sign({ id: user._id }, 'help'), admin: user.admin })
+        console.log(user)
+        res.json({ isLoggedIn: !!user, user: user.username, token: jwt.sign({ id: user._id }, 'help'), admin: user.admin , id:user._id})
       })
     })
   })
   app.post('/login', (req, res) => {
     User.authenticate()(req.body.username, req.body.password, (e, user) => {
       if (e) throw e
-      res.json({ isLoggedIn: !!user, user: user.username, token: jwt.sign({ id: user._id }, 'help'), admin: user.admin })
+      console.log(user)
+
+      res.json({ isLoggedIn: !!user, user: user.username, token: jwt.sign({ id: user._id }, 'help'), admin: user.admin, id:user._id })
     })
   })
   app.put('/user/:id', (req, res) => {
@@ -31,8 +34,8 @@ module.exports = app => {
       .catch(e => console.log(e))
   })
   app.get('/user/watchlist/:id', (req, res) => {
-    User.findById({ _id: req.params.id }, req.body)
-    .then(_ => res.sendStatus(200))
+    User.findById(req.params.id)
+    .then(r=> res.json(r))
     .catch(e => console.log(e))
   })
  
