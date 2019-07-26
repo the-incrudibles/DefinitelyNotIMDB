dddafe7749e762e91cb828fd12716cf8c5f9b58a
Mahya <safavis@uci.edu>
2019-07-25 21:54:12 -0700

    populating the screen

--- client/src/components/MyWatchList/MyWatchList.js
+++ client/src/components/MyWatchList/MyWatchList.js
@@ -12,15 +12,30 @@ import WatchlistContext from '../../utils/Watchlist.js'
 const MyWatchList = _ => {
   const [watchListState, setwatchListState] = useState(
     {
-      watchList: []
+      watchList: [],
+     
     }
   )
  
   useEffect(_ => {
     WatchlistContext.getWatchlist(localStorage.getItem('id'))
       .then(r => {
-        setwatchListState({ ...watchListState,watchList: r.watchlist })
-      })
+        let fetchURL=[]
+        let title=[]
+        let fetchedMovieArr=[]
+        r.map(elem=>{
+         axios.get(`https://api.themoviedb.org/3/movie/${parseInt(elem.watchlist)}?api_key=${process.env.REACT_APP_TMDB_APIKEY}&language=en-US`)
+        .then(result=>{
+            fetchURL=('https://image.tmdb.org/t/p/original' +result.data.results.poster_path)
+            title=(result.data.results.title)
+            })
+        .catch(e=>console.log(e))
+          })
+          let fetchedMovie={id:r.watchlist,posterURL:fetchURL,title:title}
+          fetchedMovieArr.push(fetchedMovie)
+          setwatchListState({ ...watchListState,watchList: fetchedMovie})
+
+       })
       .catch(e => console.error(e))
   }, [])
   
@@ -29,11 +44,14 @@ const MyWatchList = _ => {
       let newWatchList=watchListState.watchList
       newWatchList.push(movieId)
       setwatchListState({ ...watchListState,watchList: newWatchList })
-      WatchlistContext.addWatchlist()
+      WatchlistContext.addWatchlist(watchListState.watchList)
 
   }
-  watchListState.removeWatchList=userId=>{
-    
+  watchListState.removeWatchList=movieId=>{
+    let newWatchList=watchListState.watchList
+      newWatchList.push(movieId)
+      setwatchListState({ ...watchListState,watchList: newWatchList })
+      WatchlistContext.addWatchlist(watchListState.watchList)
   }
   // const cards = [1, 2, 3, 4, 5, 6]
   return (
@@ -44,7 +62,7 @@ const MyWatchList = _ => {
           <Typography variant='h6'>Your Watchlist</Typography>
         </div>
         {
-          watchListState.watchList.map(elem => {
+          watchListState.watchList.map(movieElem => {
             return (
               <>
             <Card className='resultsDiv'>
@@ -53,7 +71,7 @@ const MyWatchList = _ => {
                   <Typography gutterBottom variant='h6' component='h2'>
                     Title
                   </Typography>
-                  <img className='resultsPoster' src={'https://image.tmdb.org/t/p/original/xRWht48C2V8XNfzvPehyClOvDni.jpg'} alt='title' />
+                  <img className='resultsPoster' src={movieElem.posterURL} alt='title' />
                   <div className='cardTypography'>
                     <Typography variant='body2' color='textSecondary' component='p'>
                       Mia, an aspiring actress, serves lattes to movie stars in between auditions and Sebastian, a jazz musician, scrapes by playing cocktail party gigs in dingy bars, but as success mounts they are faced...
@@ -62,10 +80,10 @@ const MyWatchList = _ => {
                 </CardContent>
               </CardActionArea>
               <CardActions>
-                <Button size='small' color='primary' onClick={watchListState.addWatchList()}>
+                <Button size='small' color='primary' onClick={watchListState.addWatchList(movieElem.id)}>
                   Add to Watchlist
                 </Button>
-                <Button size='small' color='primary' onClick={watchListState.removeWatchList()}>
+                <Button size='small' color='primary' onClick={watchListState.removeWatchList(movieElem.id)}>
                   Remove from Watchlist
                 </Button>
               </CardActions>
