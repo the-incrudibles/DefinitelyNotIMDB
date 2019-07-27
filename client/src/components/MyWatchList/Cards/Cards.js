377918dca2dbc2ceff99684ce530de26b8494a55
Pandapoc <45634681+Pandapoc@users.noreply.github.com>
2019-07-26 23:41:34 -0700

    test fixes

--- client/src/components/MyWatchList/Cards/Cards.js
+++ client/src/components/MyWatchList/Cards/Cards.js
@@ -18,7 +18,7 @@ const Cards = _ => {
         let index = data.watchlist.indexOf(String(mID))
         let newWatchlist = data.watchlist.splice(index, 1)
         axios.put(`/user/${localStorage.getItem('id')}`, { watchlist: data.watchlist })
-          .then(_ => window.location.reload())
+          .then(_ => window.location.reload(true))
           .catch(e => console.log(e))
       })
       .catch(e => console.log(e))
